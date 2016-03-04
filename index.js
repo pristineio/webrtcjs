329386f03e30651e499ccb5701b4a6b46146b5ad
Ben <ben@ps.gs>
2016-03-04 10:53:36 -0600

    Clean up

--- index.js
+++ index.js
@@ -2,58 +2,48 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var WebSocket = require('wsd');
 var webrtcjs = require('./build/Debug/webrtcjs.node');
-
 var ws = new WebSocket('wss://localhost:3232/ws?token=bob');
 
-// var constraints = {
-//   mandatory: {
-//     OfferToReceiveAudio: true,
-//     OfferToReceiveVideo: true
-//   }
+var pcConfig = {iceServers: [{url: 'stun:stun.l.google.com:19302'}]};
+var pcConstraints = {mandatory: {
+  OfferToReceiveAudio: false,
+  OfferToReceiveVideo: true
+}};
+
+// var aliceVideoTrack;
+// var aliceVideoSink = new webrtcjs.VideoSink();
+// aliceVideoSink.onframe = function(data) {
+//   console.log('onframe', JSON.stringify(data));
 // };
 
-var aliceVideoTrack;
-var aliceVideoSink = new webrtcjs.VideoSink();
-aliceVideoSink.onframe = function(data) {
-  console.log('onframe', JSON.stringify(data));
+var bob = new webrtcjs.RTCPeerConnection(); //pcConfig, pcConstraints);
+
+bob.onicecandidate = function(e) {
+  ws.send(JSON.stringify({to: 'alice', type: 'ICE', payload: e.candidate}));
 };
 
-var bob = new webrtcjs.RTCPeerConnection();
-
-bob.onicecandidate = function(event) {
-  var candidate = event.candidate || event;
-  ws.send(JSON.stringify({to: 'alice', type: 'ICE', payload: candidate}));
+bob.onaddstream = function(aliceStream) {
+  // aliceVideoTrack = aliceStream.getVideoTracks()[0];
+  // aliceVideoTrack.addSink(aliceVideoSink);
+  bob.addStream(aliceStream);
+  bob.createAnswer(function(bobSdp) {
+    console.log('    Bob: createAnswer ' + JSON.stringify(bobSdp));
+    ws.send(JSON.stringify({to: 'alice', type: 'ANSWER', payload: bobSdp}));
+    bob.setLocalDescription(bobSdp, function() {
+      console.log('    Bob: setLocalDescription');
+    });
+  });
 };
 
-bob.onaddstream = function(stream) {
-  aliceVideoTrack = stream.getVideoTracks()[0];
-  aliceVideoTrack.addSink(aliceVideoSink);
-  console.log('\n\n\n');
-  console.log(aliceVideoTrack);
-  console.log('\n\n\n');
-};
 
 ws.on('message', function(data) {
   var message = JSON.parse(data);
-  console.log('\n\n\n');
   console.log(message);
-  console.log('\n\n\n');
 
   switch(message.type) {
     case 'OFFER':
       bob.setRemoteDescription(message.payload, function() {
         console.log('    Bob: setRemoteDescription');
-        bob.createAnswer(function(bobSdp) {
-          console.log('    Bob: createAnswer ' + JSON.stringify(bobSdp));
-          ws.send(JSON.stringify({
-            to: 'alice',
-            type: 'ANSWER',
-            payload: bobSdp
-          }));
-          bob.setLocalDescription(bobSdp, function() {
-            console.log('    Bob: setLocalDescription');
-          });
-        });
       });
       break;
 
@@ -76,7 +66,7 @@ ws.on('message', function(data) {
 // //   });
 // // }, 1000);
 
-// bob.createOffer(function(aliceSdp) {
+// alice.createOffer(function(aliceSdp) {
 //   console.log('    Alice: createOffer ' + JSON.stringify(aliceSdp));
 //   alice.setLocalDescription(aliceSdp, function() {
 //     console.log('    Alice: setLocalDescription');
